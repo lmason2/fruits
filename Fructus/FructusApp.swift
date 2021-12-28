455ac9b8a30827bddd8b17073dcef80e9221d0b7
Luke Mason <lukesamuelmason@gmail.com>
2021-12-28 12:22:17 -0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Luke Mason on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
